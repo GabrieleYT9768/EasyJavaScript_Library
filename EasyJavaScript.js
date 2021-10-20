853812230fa92a8146d04a869cceae113d1929b9
GabrieleYT9768 <honorgabrielegg@gmail.com>
2021-10-20 18:45:30 +0200

    Update EasyJavaScript.js

--- EasyJavaScript.js
+++ EasyJavaScript.js
@@ -22,152 +22,161 @@ var library = {
 	}
 }
 
-var easyJavaScriptLibrary_songs = [];
+// var easyJavaScriptLibrary_songs = [];
 
-// copy() Function
-function copy(text) {
-	let textArea = document.createElement('textarea');
-	document.body.appendChild(textArea);
-	textArea.value = String(text);
-	textArea.select();
-	document.execCommand('copy');
-	document.body.removeChild(textArea);
-}
+const easyjs = {
 
-// random() Function
-function random(n) {
-	return Math.round( Math.random() * parseInt(n) );
-}
+	// copy() Function
+	copy: function(text) {
+		let textArea = document.createElement('textarea');
+		document.body.appendChild(textArea);
+		textArea.value = String(text);
+		textArea.select();
+		document.execCommand('copy');
+		document.body.removeChild(textArea);
+	},
 
-// clock() Function
-function clock() {
-	let date = new Date();
-	let h = String(date.getHours());
-	if (h.length == 1) {
-		h = '0' + h;
+	// random() Function
+	random: function(n) {
+		return Math.round( Math.random() * parseInt(n) );
+	},
+
+	// clock() Function
+	clock: function() {
+		let date = new Date();
+		let h = String(date.getHours());
+		if (h.length == 1) {
+			h = '0' + h;
+		}
+		let m = String(date.getMinutes());
+		if (m.length == 1) {
+			m = '0' + m;
+		}
+		let s = String(date.getSeconds());
+		if (s.length == 1) {
+			s = '0' + s;
+		}
+		return h + ':' + m + ':' + s;
+	},
+
+	doc: {
+
+		id: function(i) {
+			return document.getElementById(String(i));
+		},
+
+		tag: function(t) {
+			return document.getElementsByTagName(String(t))[parseInt(n)];
+		},
+		
+		class: function(c) {
+			return document.getElementsByClassName(String(c))[parseInt(n)];
+		}
+
+	},
+
+	s: {
+
+		block: {
+
+			id: function(i) {
+				document.getElementById(String(i)).style.display = 'block';
+			},
+
+			tag: function(t) {
+				document.getElementByTag(String(t)).style.display = 'block';
+			},
+
+			class: function(c) {
+				document.getElementsByClassName(String(c)).style.display = 'block'
+			}
+
+		},
+
+		color: {
+
+			id: function(i, color) {
+				document.getElementById(i).style.color = String(color);
+			},
+
+			tag: function(t, color) {
+				document.getElementsByTagName(t).style.color = String(color);
+			},
+
+			class: function(c, color) {
+				document.getElementsByClassName(c).style.color = String(color);
+			}
+
+		},
+
+		bgColor: {
+
+			id: function(i, color) {
+				document.getElementById(i).style.backgroundColor = String(color);
+			},
+
+			tag: function(t, color) {
+				document.getElementsByTagName(t).style.backgroundColor = String(color);
+			},
+
+			class: function(c, color) {
+				document.getElementsByClassName(c).style.backgroundColor = String(color);
+			}
+
+		},
+
+		font: {
+
+			id: function(i, font) {
+				document.getElementById(i).style.fontFamily = String(font);
+			},
+
+			tag: function(t, font) {
+				document.getElementsByTagName(t).style.fontFamily = String(font);
+			},
+
+			class: function(c, font) {
+				document.getElementsByClassName(c).style.fontFamily = String(font);
+			}
+
+		},
+
+		none: {
+
+			id: function(i) {
+				document.getElementById(String(i)).style.display = 'none';
+			},
+
+			tag: function(t) {
+				document.getElementByTag(String(t)).style.display = 'none';
+			},
+
+			class: function(c) {
+				document.getElementsByClassName(String(c)).style.display = 'none'
+			}
+
+		},
+	
+	},
+
+	// randomColor() Function
+	randomColor: function() {
+		return 'rgb(' + random(255) + ', ' + random(255) + ', ' + random(255) + ')';
+	},
+
+	// date() Function
+	date: function() {
+		let date = new Date();
+		let d = String(date.getDate());
+		if (d.length == 1) {
+			d = '0' + d;
+		}
+		let m = String(date.getMonth());
+		if (m.length == 1) {
+			m = '0' + m;
+		}
+		let y = String(date.getFullYear());
+		return d + '/' + m + '/' + y;
 	}
-	let m = String(date.getMinutes());
-	if (m.length == 1) {
-		m = '0' + m;
-	}
-	let s = String(date.getSeconds());
-	if (s.length == 1) {
-		s = '0' + s;
-	}
-	return h + ':' + m + ':' + s;
-}
 
-// docId() Function
-function docId(i) {
-	return document.getElementById(String(i));
-}
-
-// docTag() Function
-function docTag(t, n) {
-	return document.getElementsByTagName(String(t))[parseInt(n)];
-}
-
-// docClass() Function
-function docClass(c, n) {
-	return document.getElementsByClassName(String(c))[parseInt(n)];
-}
-
-// blockById() Function
-function blockById(i) {
-	return docId(String(i)).style.display = 'block';
-}
-
-// noneById() Function
-function noneById(i) {
-	return docId(String(i)).style.display = 'none';
-}
-
-// blockByTag() Function
-function blockByTag(i, n) {
-	return docTag(String(i), parseInt(n)).style.display = 'block';
-}
-
-// noneByTag() Function
-function noneByTag(i, n) {
-	return docTag(String(i), parseInt(n)).style.display = 'none';
-}
-
-// blockByClass() Function
-function blockByClass(i, n) {
-	return docClass(String(i), parseInt(n)).style.display = 'block';
-}
-
-// noneByClass() Function
-function noneByClass(i, n) {
-	return docClass(String(i), parseInt(n)).style.display = 'none';
-}
-
-// randomColor() Function
-function randomColor() {
-	return 'rgb(' + random(255) + ', ' + random(255) + ', ' + random(255) + ')';
-}
-
-//  date() Function
-function date() {
-	let date = new Date();
-	let d = String(date.getDate());
-	if (d.length == 1) {
-		d = '0' + d;
-	}
-	let m = String(date.getMonth());
-	if (m.length == 1) {
-		m = '0' + m;
-	}
-	let y = String(date.getFullYear());
-	return d + '/' + m + '/' + y;
-}
-
-// colorById() Function
-function colorById(i, color) {
-	docId(i).style.color = String(color);
-}
-
-// colorByTag() Function
-function colorByTag(t, n, color) {
-	docTag(t, n).style.color = String(color);
-}
-
-// colorByClass() Function
-function colorByClass(c, n, color) {
-	docClass(c, n).style.color = String(color);
-}
-
-// bgColorById() Function
-function bgColorById(i, color) {
-	docId(i).style.backgroungColor = String(color);
-}
-
-// bgColorByTag() Function
-function bgColorByTag(t, n, color) {
-	docTag(t, n).style.backgroundColor = String(color);
-}
-
-// bgColorByClass() Function
-function bgColorByClass(c, n, color) {
-	docClass(c, n).style.backgroundColor = String(color);
-}
-
-// fontById() Function
-function fontById(i, f) {
-	docId(i).style.fontFamily = String(f);
-}
-
-// fontByClass() Function
-function fontByClass(c, n, f) {
-	docClass(c, n).style.fontFamily = String(f);
-}
-
-// fontByTag() Function
-function fontByTag(t, n, f) {
-	docTag(t, n).style.fontFamily = String(f);
-}
-
-// musicPlay() Function
-function musicPlay(sN, sL) {
-	//let s = 'songName_' + Math.random().toString(36).replace(/[^a-z]+/g, '');
 }
